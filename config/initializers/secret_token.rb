762e3d2894041970a16fd9b50e25ef0ff8fa5563
kghosh <kaushik_ghosh@intuit.com>
2013-03-06 17:22:52 -0800

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareOffice2::Application.config.secret_token = 'caed72d806ddd9becd376cae337f0164c72baef7c63cbd0e21d3b1603047dc0d504f08dafc77c38701f79f9d3c7a2b3f371dcc6a363d01adc24edcf87222b5f9'
