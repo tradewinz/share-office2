abc87f68ea55e003c3f6a54b899f7b39a50dd932
kghosh <kaushik_ghosh@intuit.com>
2013-03-08 09:30:33 -0800

    homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+  	@rentestimate = Rentestimate.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @rentestimate }
+    end
   end
 
   def help
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 ShareOffice2::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
-
-  get "static_pages/about"
 
   resources :rentestimates
 
+
+  match '/home', to: 'static_pages#home'
+
+  match '/help', to: 'static_pages#help'
+
+  match '/about', to:'static_pages#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
